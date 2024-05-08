9bf9cb73d4e9d84dad84336a8d7b90c777a29810
Alan Jeferson Macias Mamani <alanmacias2340@gmail.com>
2024-05-07 21:42:53 -0400

    nuevo ejercicio de arrays, nuemor 9

---
+++ 06_arrays/09-join.js
@@ -0,0 +1,10 @@
+let arr1 = ['Nicolas','chanchito','alan'];
+
+let mensaje = arr1.join(', ') //Une arrays
+
+// console.log(mensaje);
+
+let saludo = "Hola mundo";
+
+let dividido = saludo.split(" "); // divide una cadena de texto en arrays
+console.log(dividido.join('-'));
